b0967af2ecb262ba88e14797479b286187f3c28b
Nace Logar <the.code.destroyer@gmail.com>
2016-03-23 14:10:52 +0100

    Refactored the task

--- gulp/tasks/babel.js
+++ gulp/tasks/babel.js
@@ -1,7 +1,20 @@
 import gulp from 'gulp';
-import shell from 'gulp-shell';
+import babel from 'gulp-babel';
+import plumber from 'gulp-plumber';
+import sourcemaps from 'gulp-sourcemaps';
+import notify from 'gulp-notify';
+import objectAssign from 'object.assign';
 import paths from '../paths';
+import babelOptions from '../babelOptions';
 
-gulp.task('babelServer', shell.task([
-    `babel ${paths.srcServer} --out-dir ${paths.destServer} --sourceRoot=${paths.srcServer}`
-]));
+const assign = Object.assign || objectAssign;
+
+gulp.task('babelServer', () => {
+
+    return gulp.src(paths.srcServerJs)
+    .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
+    .pipe(babel(assign({}, babelOptions.server())))
+    .pipe(sourcemaps.write({includeContent: false, sourceRoot: '/src'}))
+    .pipe(gulp.dest(paths.destServer));
+});
+
