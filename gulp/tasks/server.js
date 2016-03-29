81d0f7d35732842815035cd1d719d82ec33804b4
Nace Logar <the.code.destroyer@gmail.com>
2016-03-29 15:18:46 +0200

    Changed path of server start

--- gulp/tasks/server.js
+++ gulp/tasks/server.js
@@ -5,7 +5,7 @@ import paths from '../paths';
 let express;
 
 gulp.task('server', () => {
-    express = server.new(paths.destServer);
+    express = server.new(paths.dest);
 });
 
 gulp.task('restart', () => {
