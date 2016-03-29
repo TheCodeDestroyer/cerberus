c04e89872237cf231e2525830a22655251e3d79a
Nace Logar <the.code.destroyer@gmail.com>
2016-03-29 15:19:18 +0200

    Changed path to public folder

--- src/app/index.js
+++ src/app/index.js
@@ -8,7 +8,7 @@ const app = express();
 // const dev = env === 'development';
 const port = process.env.PORT || 8080;
 
-app.use(express.static(`${__dirname}/../public`));
+app.use(express.static(`${__dirname}/public`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
