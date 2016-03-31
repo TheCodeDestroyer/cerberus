56e093125bd1d76fbffd259f1b95824fe4b8451d
Nace Logar <the.code.destroyer@gmail.com>
2016-03-31 09:03:10 +0200

    Changed how express is started

--- src/server/index.js
+++ src/server/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import morgan from 'morgan';
+import yargs from 'yargs';
 
-const app = express();
-// const env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-// const dev = env === 'development';
+const argv = yargs.argv;
+const currentEnv = process.env.NODE_ENV = argv.env || process.env.NODE_ENV || 'dev';
+const dev = currentEnv === 'dev';
 const port = process.env.PORT || 8080;
+let app = express();
 
 app.use(express.static(`${__dirname}/public`));
 app.use(bodyParser.json());
@@ -14,6 +16,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan('dev'));
 
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-});
+if (!dev) {
+    app.listen(port, () => {
+        console.log(`Server is listening on port ${port}`);
+    });
+}
+
+export default app;
