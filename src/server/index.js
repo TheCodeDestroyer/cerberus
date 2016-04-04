7bd884fbd0d28f8cecfff0786a3799fe25c083f0
Nace Logar <the.code.destroyer@gmail.com>
2016-04-04 15:28:45 +0200

    Cleaned up server index

--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,8 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import morgan from 'morgan';
 import yargs from 'yargs';
-import shell from 'shelljs';
+
+import controllers from './controllers';
 
 const argv = yargs.argv;
 const currentEnv = process.env.NODE_ENV = argv.env || process.env.NODE_ENV || 'dev';
@@ -16,25 +17,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan('dev'));
 
+app.use(controllers);
+
 app.listen(port, () => {
     console.log(`${currentEnv.toUpperCase()} - Server is listening on port ${port}`);
 });
-
-app.get('/kodi', function(req, res) {
-    let result = shell.exec('runKodi').output;
-    console.log(result);
-    res.send({ result: result });
-});
-
-app.get('/moonlight', function(req, res) {
-    let result = shell.exec('runKodi').output;
-    console.log(result);
-    res.send({ result: result });
-});
-
-app.get('/nodeVersion', function(req, res) {
-    let result = shell.exec('node --version').output;
-    console.log(result);
-    res.send({ result: result });
-});
-
