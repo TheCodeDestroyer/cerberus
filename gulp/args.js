d485bd8de2753b79c2e3758fd9734229f8fba71f
Nace Logar <the.code.destroyer@gmail.com>
2016-04-07 20:12:21 +0200

    Created argument common module

---
+++ gulp/args.js
@@ -0,0 +1,11 @@
+import yargs from 'yargs';
+
+const argv = yargs.argv;
+const validBumpTypes = 'major|minor|patch|prerelease'.split('|');
+const bump = (argv.bump || 'patch').toLowerCase();
+
+if (validBumpTypes.indexOf(bump) === -1) {
+  throw new Error('Unrecognized bump "' + bump + '".');
+}
+
+export default bump;
