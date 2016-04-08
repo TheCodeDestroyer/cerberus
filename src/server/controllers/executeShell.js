e0bf78ace49eb8bbcf4383d322b71d625d93bbe1
Nace Logar <the.code.destroyer@gmail.com>
2016-04-08 20:23:57 +0200

    Implemented push events for console output and changed http request response

--- src/server/controllers/executeShell.js
+++ src/server/controllers/executeShell.js
@@ -1,20 +1,19 @@
 import express from 'express';
 import shell from 'shelljs';
+import {io} from '../index';
 
 let router = express.Router();
 
 router.get('/:rawCommand', function(req, res) {
     let command = req.params.rawCommand;
-
     executeShell(command);
-
-    res.send({ execResult: `Executed ${command}!` });
+    res.send({ timestamp: new Date().getTime(), output: `Executed ${command}!` });
 });
 
 let executeShell = shellCommand => {
     let childProcess = shell.exec(shellCommand, { async: true });
     childProcess.stdout.on('data', (data) => {
-        //TODO: Push to client
+        io.sockets.emit('shellLog', { timestamp: new Date().getTime(), output: data.toString() });
     });
 };
 
