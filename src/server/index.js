02a15e72cbace9658d37f687ce0dce2a6ca5e503
Nace Logar <the.code.destroyer@gmail.com>
2016-04-08 20:23:13 +0200

    Extended index to support socket.io

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,14 @@
 import express from 'express';
+import http from 'http';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import socket from 'socket.io';
 import morgan from 'morgan';
 import yargs from 'yargs';
+
+let app = express();
+let server = http.createServer(app);
+export let io = socket(server);
 
 //APP COMPONENTS
 import controllers from './controllers';
@@ -10,7 +16,6 @@ import controllers from './controllers';
 const argv = yargs.argv;
 const currentEnv = process.env.NODE_ENV = argv.env || process.env.NODE_ENV || 'dev';
 const port = process.env.PORT || 8080;
-let app = express();
 
 app.use(express.static(`${__dirname}/public`));
 app.use(bodyParser.json());
@@ -20,6 +25,15 @@ app.use(morgan('dev'));
 
 app.use(controllers);
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`${currentEnv.toUpperCase()} - Server is listening on port ${port}`);
 });
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
+
+export default app;
