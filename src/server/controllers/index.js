aa626423c22c637bc55927b3bb201d7397b921cc
Nace Logar <the.code.destroyer@gmail.com>
2016-04-26 22:36:36 +0200

    Added script route usage to app

--- src/server/controllers/index.js
+++ src/server/controllers/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import executeShell from './executeShell';
+import script from './script';
 
 let router = new express.Router();
 
 router.use('/executeShell', executeShell);
+router.use('/script', script);
 
 export default router;
