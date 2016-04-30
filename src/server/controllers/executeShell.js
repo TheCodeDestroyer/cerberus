92205ba099a8b8d4b45d41ca2699782cffe0964b
Nace Logar <the.code.destroyer@gmail.com>
2016-04-30 12:59:33 +0200

    Changed to execution by name and added storage querying by script name

--- src/server/controllers/executeShell.js
+++ src/server/controllers/executeShell.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import shellUtil from '../helpers/shellUtil';
+import Script from '../models/Script';
 
 let router = new express.Router();
 
-router.get('/:rawCommand', (req, res) => {
-    const command = req.params.rawCommand;
-    let processInfo = shellUtil.executeShell(command);
-    res.send({ success: true, processId: processInfo.processId, shellName: processInfo.shellName });
+router.get('/:scriptName', (req, res) => {
+    const scriptName = req.params.scriptName;
+
+    Script.findOne({ name: scriptName }, (err, script) => {
+        let processInfo = shellUtil.executeShell(script);
+        res.send({ success: true, processId: processInfo.processId, shellName: processInfo.shellName });
+    });
 });
 
 export default router;
