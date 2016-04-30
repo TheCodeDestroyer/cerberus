6ea15ee1df889efbed95da35d901fe7607dc24f8
Nace Logar <the.code.destroyer@gmail.com>
2016-04-30 16:22:12 +0200

    Implemented script detail functionality for creation and modification

--- src/app/script/scriptDetail.js
+++ src/app/script/scriptDetail.js
@@ -1,13 +1,14 @@
 import {inject} from 'aurelia-framework';
-import {HttpClient} from 'aurelia-fetch-client';
+import {HttpClient, json} from 'aurelia-fetch-client';
+import {Router} from 'aurelia-router';
 import 'whatwg-fetch';
 
-@inject(HttpClient)
+@inject(HttpClient, Router, json)
 export class ScriptDetail {
     heading = 'Script Detail';
-    scriptName = '';
+    scriptModel = {};
 
-    constructor(http) {
+    constructor(http, router, json) {
         http.configure(config => {
             config
             .useStandardConfiguration()
@@ -15,10 +16,53 @@ export class ScriptDetail {
         });
 
         this.http = http;
+        this.router = router;
+        this.json = json;
     }
-    
+
     activate(params) {
-        this.scriptName = params.name; 
+        const scriptId = params.id;
+
+        this.scriptModel.name = 'New';
+
+        if (scriptId.length > 2) {
+            this.http.fetch(`/${scriptId}`)
+            .then(response => response.json())
+            .then(responseObject => {
+                this.scriptModel = responseObject.data;
+                console.log(responseObject);
+            });
+        }
+    }
+
+    submit() {
+        const scriptModel = this.scriptModel;
+        let methodType = 'post';
+        let uri = '';
+        if (scriptModel._id) {
+            methodType = 'put';
+            uri = `/${scriptModel._id}`
+        }
+
+        this.http.fetch(uri, {
+            method: methodType,
+            body: json(scriptModel)
+        })
+        .then(() => {
+            this.router.navigate('');
+        });
+    }
+
+    cancel() {
+        this.router.navigate('')
+    }
+
+    saveScript() {
+
+    }
+
+    updateScript() {
+
     }
 
 }
