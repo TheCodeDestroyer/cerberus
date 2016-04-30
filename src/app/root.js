699e4609bd0d71e67a6b889d6c9f4ce7fe8dcb7f
Nace Logar <the.code.destroyer@gmail.com>
2016-04-30 16:20:58 +0200

    Changed from name to id and removed home from nav

--- src/app/root.js
+++ src/app/root.js
@@ -2,8 +2,8 @@ export class Root {
     configureRouter(config, router) {
         config.title = 'Cerberus';
         config.map([
-            { route: ['', 'home'], name: 'home', moduleId: './home/list', nav: true, title: 'Home' },
-            { route: 'script/:name', name: 'script', moduleId: './script/scriptDetail', nav: false, title: 'Script' }
+            { route: ['', 'home'], name: 'home', moduleId: './home/list', nav: false, title: 'Home' },
+            { route: 'script/:id', name: 'script', moduleId: './script/scriptDetail', nav: false, title: 'Script' }
         ]);
 
         this.router = router;
