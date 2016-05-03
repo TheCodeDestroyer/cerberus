450b2752731191030a7aafe8bf3da7853390b0ef
Nace Logar <the.code.destroyer@gmail.com>
2016-05-03 21:24:19 +0200

    Added user controller

--- src/server/controllers/index.js
+++ src/server/controllers/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 
 import executeShell from './executeShell';
 import script from './script';
+import user from './user';
 
 let router = new express.Router();
 const apiRoot = '/api';
 
 router.use(`${apiRoot}/executeShell`, executeShell);
 router.use(`${apiRoot}/script`, script);
+router.use(`${apiRoot}/user`, user);
 
 export default router;
