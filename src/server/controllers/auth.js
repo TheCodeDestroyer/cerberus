fe5af7c1e99c9a721d349b03bcdd97a6ecad667b
Nace Logar <the.code.destroyer@gmail.com>
2016-05-08 11:59:01 +0200

    Added more strategies

--- src/server/controllers/auth.js
+++ src/server/controllers/auth.js
@@ -1,7 +1,10 @@
 import passport from 'passport';
 import {BasicStrategy} from 'passport-http';
+import {Strategy as BearerStrategy} from 'passport-http-bearer';
 
 import User from '../models/user';
+import ApplicationClient from '../models/applicationClient';
+import Token from '../models/token';
 
 passport.use(new BasicStrategy((username, password, cb) => {
         User.findOne({ username: username }, function (err, user) {
@@ -20,6 +23,34 @@ passport.use(new BasicStrategy((username, password, cb) => {
     }
 ));
 
-let isAuthenticated = passport.authenticate('basic', { session: false });
+passport.use('client-basic', new BasicStrategy((username, password, callback) => {
+        ApplicationClient.findOne({ id: username }, (err, client) => {
+            if (err) { return callback(err); }
 
-export default isAuthenticated;
+            if (!client || client.secret !== password) { return callback(null, false); }
+
+            return callback(null, client);
+        });
+    }
+));
+
+passport.use(new BearerStrategy((accessToken, callback) => {
+        Token.findOne({value: accessToken }, (err, token) => {
+            if (err) { return callback(err); }
+
+            if (!token) { return callback(null, false); }
+
+            User.findOne({ _id: token.userId }, (err, user) => {
+                if (err) { return callback(err); }
+
+                if (!user) { return callback(null, false); }
+
+                callback(null, user, { scope: '*' });
+            });
+        });
+    }
+));
+
+export let isAuthenticated = passport.authenticate(['basic', 'bearer'], { session : false });
+
+export let isClientAuthenticated = passport.authenticate('client-basic', { session: false });
