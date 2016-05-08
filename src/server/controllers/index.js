f0591c2abbeda1378c9c0fbd33e73e53b99125ed
Nace Logar <the.code.destroyer@gmail.com>
2016-05-08 12:02:09 +0200

    Added new controllers to index

--- src/server/controllers/index.js
+++ src/server/controllers/index.js
@@ -3,6 +3,8 @@ import express from 'express';
 import executeShell from './executeShell';
 import script from './script';
 import user from './user';
+import applicationClient from './applicationClient';
+import oauth2 from './oauth2';
 
 let router = new express.Router();
 const apiRoot = '/api';
@@ -10,5 +12,7 @@ const apiRoot = '/api';
 router.use(`${apiRoot}/executeShell`, executeShell);
 router.use(`${apiRoot}/script`, script);
 router.use(`${apiRoot}/user`, user);
+router.use(`${apiRoot}/applicationClient`, applicationClient);
+router.use(`${apiRoot}/oauth2`, oauth2);
 
 export default router;
