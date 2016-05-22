e31939fca9096c28eb51390cfde25de5798e3376
Nace Logar <the.code.destroyer@gmail.com>
2016-05-22 18:09:05 +0200

    Removed deleted controllers

--- src/server/controllers/index.js
+++ src/server/controllers/index.js
@@ -3,8 +3,6 @@ import express from 'express';
 import executeShell from './executeShell';
 import script from './script';
 import user from './user';
-import applicationClient from './applicationClient';
-import oauth2 from './oauth2';
 
 let router = new express.Router();
 const apiRoot = '/api';
@@ -12,7 +10,5 @@ const apiRoot = '/api';
 router.use(`${apiRoot}/executeShell`, executeShell);
 router.use(`${apiRoot}/script`, script);
 router.use(`${apiRoot}/user`, user);
-router.use(`${apiRoot}/applicationClient`, applicationClient);
-router.use(`${apiRoot}/oauth2`, oauth2);
 
 export default router;
