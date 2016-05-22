ae2ba0a9ea18ea6d6693dfca26bafdcd281a4a62
Nace Logar <the.code.destroyer@gmail.com>
2016-05-22 18:23:22 +0200

    Added auth as a route

--- src/server/controllers/index.js
+++ src/server/controllers/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import executeShell from './executeShell';
 import script from './script';
 import user from './user';
+import auth from './auth';
 
 let router = new express.Router();
 const apiRoot = '/api';
@@ -10,5 +11,6 @@ const apiRoot = '/api';
 router.use(`${apiRoot}/executeShell`, executeShell);
 router.use(`${apiRoot}/script`, script);
 router.use(`${apiRoot}/user`, user);
+router.use(`${apiRoot}/auth`, auth);
 
 export default router;
