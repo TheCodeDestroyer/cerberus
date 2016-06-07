717d40ef209e7981ca024de871ce05d3873305c0
Nace Logar <the.code.destroyer@gmail.com>
2016-06-07 20:06:25 +0200

    Added more configs

--- src/app/common/constants/authConfig.js
+++ src/app/common/constants/authConfig.js
@@ -1,7 +1,10 @@
+//TODO: Use proper values for dev and production
 let config = {
+    baseUrl: 'http://localhost:8080',
     loginUrl: 'api/auth',
     tokenName: 'cerberus_token',
-    loginRedirect: '#/'
+    loginRedirect: '#/',
+    responseTokenProp: 'token'
 };
 
 export default config;
